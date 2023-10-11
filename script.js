898922c61de11c90788c8ce24fa7c481b622a95c
Siddharth Kamboj <siddharth.techbit@outlook.com>
2023-10-11 18:07:23 +0530

    added the harry potter theme

--- script.js
+++ script.js
@@ -11,15 +11,21 @@ const ui = readline.createInterface({
 const openai = new OpenAI(
     { apiKey: process.env.gpt_api }
 );
-let convo = [];
+let convo = [{ role: "system", content: "Answer every message with a harry potter fantasy and you love harry potter movies so much" }];
+console.log(": Welcome to the Hogwards");
 ui.prompt()
 ui.on("line", async (input) => {
     convo.push({ role: "user", content: input })
-    const res = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: convo
-    })
+    const res = await get_completion(convo);
     convo.push(res.choices[0].message);
-    console.log(": ",res.choices[0].message.content);
+    console.log(": ", res.choices[0].message.content);
     ui.prompt();
 })
+
+async function get_completion(messages, temp = 0) {
+    return await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        temperature: temp
+    })
+}
