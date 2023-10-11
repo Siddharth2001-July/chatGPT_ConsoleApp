2d8e166d2db8c49034514275423882c24bf8aff6
Siddharth Kamboj <siddharth.techbit@outlook.com>
2023-10-11 16:09:20 +0530

    initial push

---
+++ script.js
@@ -0,0 +1,26 @@
+import { config } from "dotenv";
+config()
+import { OpenAI } from 'openai';
+import readline from 'readline';
+
+const ui = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const openai = new OpenAI(
+    { apiKey: process.env.gpt_api }
+);
+let convo = [];
+ui.prompt()
+ui.on("line", async (input) => {
+    convo.push({ role: "user", content: input })
+    const res = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: convo
+    })
+    convo.push(res.choices[0].message);
+    console.log(res.choices[0].message.content);
+    ui.prompt()
+})
+
